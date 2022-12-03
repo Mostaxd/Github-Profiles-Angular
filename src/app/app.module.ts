82c6be873e5f2c61a9c483b2505776d87cc8ca91
Mostafa <mostafa152804@gmail.com>
2022-12-03 23:01:53 +0100

    initialized project, Added Boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilesComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
