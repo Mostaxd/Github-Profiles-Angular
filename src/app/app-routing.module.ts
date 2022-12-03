863f43c6e536d3f817cb0331779282cce4fe240f
Mostafa <mostafa152804@gmail.com>
2022-12-03 23:33:48 +0100

    Project initialized incl, Components and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { HistoryComponent } from './history/history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProfilesComponent},
+  {path: 'profiles', component: ProfilesComponent},
+  {path: 'history', component: HistoryComponent},
+  {path: '***', component: ProfilesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
