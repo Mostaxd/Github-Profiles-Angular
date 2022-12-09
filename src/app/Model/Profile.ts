8ac51c9ea42506596ad7c1a06eaf80b8117e8c23
Mostafa <mostafa152804@gmail.com>
2022-12-09 22:40:43 +0100

    Profile Model Created

---
+++ src/app/Model/Profile.ts
@@ -0,0 +1,7 @@
+export class Profile {
+    constructor(public name: string,
+                public avatar_url: string,
+                public login: string,
+                public company: string,
+                public location: string,){}
+}
